76116b11732e008bb539978f72dd97df7359ff7a
DavidGStout <davidgstout63@gmail.com>
2018-02-11 17:12:37 -0500

    Added basic HTML, including select. CSV data is good, and in a JavaScript array, but is not functional in HTML drop down yet.

---
+++ script.js
@@ -0,0 +1,300 @@
+
+var lsz = 98;
+
+var Col0 = Array([lsz]);
+var Col1 = Array([lsz]);
+
+Col0[0] = "0001";
+Col1[0] = "Caution account";
+
+Col0[1] = "0002";
+Col1[1] = "Closed account";
+
+Col0[2] = "0003";
+Col1[2] = "Arrest";
+
+Col0[3] = "0004";
+Col1[3] = "Pick-up (Fraud Code - F1)";
+
+Col0[4] = "0005";
+Col1[4] = "Transferred Account";
+
+Col0[5] = "0006";
+Col1[5] = "Lost card - pending transfer";
+
+Col0[6] = "0007";
+Col1[6] = "Card expired";
+
+Col0[7] = "0008";
+Col1[7] = "Chargeoff account";
+
+Col0[8] = "0016";
+Col1[8] = "Account coded M9 - Closed";
+
+Col0[9] = "0020";
+Col1[9] = "Account coded FA, Fraud";
+
+Col0[10] = "0029";
+Col1[10] = "Account coded B9, Closed misc";
+
+Col0[11] = "0030";
+Col1[11] = "Account coded W1, Liability";
+
+Col0[12] = "0031";
+Col1[12] = "Account coded (Credit rating)";
+
+Col0[13] = "0032";
+Col1[13] = "Currently past due 1+ day";
+
+Col0[14] = "0033";
+Col1[14] = "Currently past due 31+ days";
+
+Col0[15] = "0034";
+Col1[15] = "Currently past due 61+ days";
+
+Col0[16] = "0035";
+Col1[16] = "Currently past due 91+ days";
+
+Col0[17] = "0036";
+Col1[17] = "Zero balance";
+
+Col0[18] = "0037";
+Col1[18] = "Caution ZIP Code";
+
+Col0[19] = "0039";
+Col1[19] = "Exceeds spending limit";
+
+Col0[20] = "0040";
+Col1[20] = "Past due 121-150 currently";
+
+Col0[21] = "0041";
+Col1[21] = "Past due 151-180 currently";
+
+Col0[22] = "0042";
+Col1[22] = "Past due 181-210 currently";
+
+Col0[23] = "0043";
+Col1[23] = "Past due 211 - 240 currently";
+
+Col0[24] = "0044";
+Col1[24] = "Past due 241-270 currently";
+
+Col0[25] = "0045";
+Col1[25] = "Past due 271-300 currently";
+
+Col0[26] = "0046";
+Col1[26] = "Past due 301-330 currently";
+
+Col0[27] = "0048";
+Col1[27] = "Not enough available money";
+
+Col0[28] = "";
+Col1[28] = "";
+
+Col0[29] = "0049";
+Col1[29] = "Excessive declined authorizations today";
+
+Col0[30] = "0058";
+Col1[30] = "Exceeds credit limit";
+
+Col0[31] = "0080";
+Col1[31] = "Coded B1";
+
+Col0[32] = "0084";
+Col1[32] = "Over limit";
+
+Col0[33] = "0085";
+Col1[33] = "Past due";
+
+Col0[34] = "0091";
+Col1[34] = "Single purchase limit exceeded";
+
+Col0[35] = "0092";
+Col1[35] = "Number of monthly transactions exceeded";
+
+Col0[36] = "0093";
+Col1[36] = "Number of daily transactions exceeded";
+
+Col0[37] = "0098";
+Col1[37] = "Closed";
+
+Col0[38] = "0101";
+Col1[38] = "Merchant suspicious";
+
+Col0[39] = "0103";
+Col1[39] = "Invalid card/account";
+
+Col0[40] = "0104";
+Col1[40] = "Less than minimum cash advance";
+
+Col0[41] = "0105";
+Col1[41] = "Exceeds maximum cash advance";
+
+Col0[42] = "0106";
+Col1[42] = "Exceeds maximum purchase limit";
+
+Col0[43] = "0107";
+Col1[43] = "Exceeds maximum mail order limit";
+
+Col0[44] = "0108";
+Col1[44] = "Suspicious activity";
+
+Col0[45] = "0109";
+Col1[45] = "PIN not allowed";
+
+Col0[46] = "0110";
+Col1[46] = "Checking/savings not allowed";
+
+Col0[47] = "0111";
+Col1[47] = "Invalid PIN";
+
+Col0[48] = "0112";
+Col1[48] = "Exceeds ATM daily limit";
+
+Col0[49] = "0113";
+Col1[49] = "Exceeds number of ATM transactions for the day";
+
+Col0[50] = "0114";
+Col1[50] = "Exceeds bad PIN limit";
+
+Col0[51] = "0115";
+Col1[51] = "Transaction amount less than ATM minimum";
+
+Col0[52] = "0116";
+Col1[52] = "Transaction $5,000 or over";
+
+Col0[53] = "0119";
+Col1[53] = "Exceeds number of authorizations for the day";
+
+Col0[54] = "0120";
+Col1[54] = "Exceeds amount of authorizations for the day";
+
+Col0[55] = "0121";
+Col1[55] = "Exceeds amount of cash available for the day";
+
+Col0[56] = "0124";
+Col1[56] = "Invalid CVV2/CVC2 value (3 digit code on back of card)";
+
+Col0[57] = "0126";
+Col1[57] = "Payment exceeds available";
+
+Col0[58] = "";
+Col1[58] = "";
+
+Col0[59] = "0127";
+Col1[59] = "Exceeds daily number cash";
+
+Col0[60] = "0128";
+Col1[60] = "Exceeds daily amount cash";
+
+Col0[61] = "0129";
+Col1[61] = "Exceeds number purchase per day";
+
+Col0[62] = "0130";
+Col1[62] = "Exceeds amount purchase per day";
+
+Col0[63] = "0134";
+Col1[63] = "Card activation";
+
+Col0[64] = "0135";
+Col1[64] = "Unable to verify PIN at this time";
+
+Col0[65] = "0158";
+Col1[65] = "Exceeds cash limit";
+
+Col0[66] = "0177";
+Col1[66] = "Exceeds bad PINs POS";
+
+Col0[67] = "0178";
+Col1[67] = "Exceeds bad PINs ATM";
+
+Col0[68] = "0179";
+Col1[68] = "Exceeds bad PINs ALL";
+
+Col0[69] = "0207";
+Col1[69] = "Invalid expiration date";
+
+Col0[70] = "0209";
+Col1[70] = "Spending limit exceeded";
+
+Col0[71] = "0210";
+Col1[71] = "Cash spending limit exceeded";
+
+Col0[72] = "0802";
+Col1[72] = "Card not effective (either no activated or expiration date input wrong)";
+
+Col0[73] = "0803";
+Col1[73] = "MCC group range excluded at individual account level";
+
+Col0[74] = "0805";
+Col1[74] = "Exceeds single transaction limit for account";
+
+Col0[75] = "0806";
+Col1[75] = "Exceeds single transaction limit for vendor";
+
+Col0[76] = "0808";
+Col1[76] = "Exceeds single transaction limit for MCC group at individual account level";
+
+Col0[77] = "0809";
+Col1[77] = "Exceeds single transaction limit for MCC group at corporate account level";
+
+Col0[78] = "0810";
+Col1[78] = "Exceeds available with Pad.";
+
+Col0[79] = "";
+Col1[79] = "";
+
+Col0[80] = "0813";
+Col1[80] = "Exceeds limit for account transaction amount/Exceeds limit for total transaction amount for the velocity";
+
+Col0[81] = "0814";
+Col1[81] = "Exceeds account limit for number of transactions received /Exceeds limit for number of transactions received at account level";
+
+Col0[82] = "0817";
+Col1[82] = "Exceeds limit for number of transactions for individual MCC group range /Exceeds limit for total number of transactions that match";
+
+Col0[83] = "0818";
+Col1[83] = "Exceeds amount limit for transactions that match individual MCC group range";
+
+Col0[84] = "";
+Col1[84] = "";
+
+Col0[85] = "0819";
+Col1[85] = "Exceeds limit for number of transactions that match corporate MCC group range";
+
+Col0[86] = "0820";
+Col1[86] = "Exceeds amount limit for transactions that match corporate MCC group range";
+
+Col0[87] = "0821";
+Col1[87] = "Monetary amount is less than the account single purchase limit";
+
+Col0[88] = "";
+Col1[88] = "";
+
+Col0[89] = "0822";
+Col1[89] = "Authorization amount does not equal the account single purchase limit or the monetary amount for the combined corporate accou";
+
+Col0[90] = "0823";
+Col1[90] = "No match found for individual account with MCC group marked include";
+
+Col0[91] = "";
+Col1[91] = "";
+
+Col0[92] = "0824";
+Col1[92] = "No match found for corporate account with MCC group marked include";
+
+Col0[93] = "";
+Col1[93] = "";
+
+Col0[94] = "0825";
+Col1[94] = "MCC group range excluded at corporate account level";
+
+Col0[95] = "0826";
+Col1[95] = "MCC group range marked for referral";
+
+Col0[96] = "0850";
+Col1[96] = "Bank Request Fraud Strategy 1";
+
+Col0[97] = "0871";
+Col1[97] = "Declined by Score 2";
+
